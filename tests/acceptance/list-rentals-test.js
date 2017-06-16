1be6a97458b4402175250dca5e29508040f15b01
Stuart Smith <stuartgordonsmith5@gmail.com>
2017-06-16 17:20:50 +0100

    added navigational tests for the about and contact pages

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,15 +1,29 @@
 import { test } from 'qunit';
-import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptance';
+import moduleForAcceptance from 'super-rentals-testing/tests/helpers/module-for-acceptance';
 
 moduleForAcceptance('Acceptance | list-rentals');
 
 test('should show rentals as the home page', function (assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(currentURL(), '/rentals', 'should redirect automatically');
+  });
 });
 
 test('should link to information about the company.', function (assert) {
+  visit('/');
+  click('a:contains("About")');
+  andThen(function() {
+    assert.equal(currentURL(), '/about', 'should navigate to about');
+  });
 });
 
 test('should link to contact information.', function (assert) {
+	visit('/contact');
+	click('a:contains("Contact")');
+	andThen(function() {
+		assert.equal(currentURL(), '/contact', 'should navigate to contact');
+	});
 });
 
 test('should list available rentals.', function (assert) {
